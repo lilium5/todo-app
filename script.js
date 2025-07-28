42b619f8b6cad6b378bee2339f4ba0b05c007318
John <admin@example.com>
2025-07-28 17:30:50 +0100

    can now remove any list item

--- script.js
+++ script.js
@@ -1,3 +1,8 @@
+const form = document.getElementById('myForm');
+    form.addEventListener('submit', function(event) {
+    event.preventDefault();}); // stops page reloading on button press
+
+
 const submit = document.getElementById('submit-btn');
 
 const myList = document.getElementById('list');
@@ -10,11 +15,18 @@ submit.addEventListener('click', function addItem() {
 
     const newNode = document.createElement('li');
     newNode.textContent = taskName;
+    
     myList.appendChild(newNode);
+
+    const newX = document.createElement('div');
+    newX.textContent = "X";
+    newX.classList.add('delete-btn');
+
+    newX.addEventListener('click', function() {
+    newNode.remove();
+});
+
+    newNode.appendChild(newX);
     
     userInput.value = "";
 });
-
-const form = document.getElementById('myForm');
-    form.addEventListener('submit', function(event) {
-    event.preventDefault();}); // stops page reloading on button press
