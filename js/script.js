2a12e8cb2ba0acc49c28418ce90c6c6e09000eb3
John <admin@example.com>
2025-08-04 10:35:38 +0100

    added edit function

--- js/script.js
+++ js/script.js
@@ -15,21 +15,34 @@ submit.addEventListener('click', function addItem() {
     if (taskName === "") return;
 
     const newNode = document.createElement('li');
-    newNode.textContent = taskName;
-    
+    const taskSpan = document.createElement('span');
+    taskSpan.textContent = taskName;
+    newNode.appendChild(taskSpan);
     myList.appendChild(newNode);
 
     newNode.addEventListener('click', function() {
         this.classList.toggle('checked')
     });
 
+    const editButton = document.createElement('div');
+    editButton.textContent = "edit";
+    editButton.classList.add('edit-btn');
+    editButton.addEventListener('click', function(event) {
+    event.stopPropagation();
+    const userEdit = prompt("Edit task:", taskSpan.textContent);
+    if (userEdit !== null && userEdit.trim() !== "") {
+        taskSpan.textContent = userEdit.trim();
+    }
+    });
+    newNode.appendChild(editButton);
+
     const deleteButton = document.createElement('div');
     deleteButton.textContent = "×";
     deleteButton.classList.add('delete-btn');
-    deleteButton.addEventListener('click', function() {
-        newNode.remove();
+    deleteButton.addEventListener('click', function(event) {
+    event.stopPropagation();
+    newNode.remove();
     });
-
     newNode.appendChild(deleteButton);
     
     userInput.value = "";
@@ -46,15 +59,14 @@ clear.addEventListener('click', function() {
 
 // create a backend proxy to fix CORS issue
 
-const quote = document.getElementById('quote-container');
+const quote = document.getElementById('quote');
+const author = document.getElementById('author');
 
 fetch('https://api.allorigins.win/raw?url=https://zenquotes.io/api/today')
     .then(response => response.json())
     .then(data => {
         console.log(data);
         quote.textContent = data[0].q;
+        author.textContent = "- " + data[0].a;
     });
-
-
- 
 
